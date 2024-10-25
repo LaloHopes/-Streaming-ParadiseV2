3190d1950d7aa8eabfab9695511bb04956d8a09c
christian cetz <cetz2702@gmail.com>
2024-10-24 18:44:14 -0600

    Creacion de rutas y modelos payment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('content_platform', 'root', '', {
+const sequelize = new Sequelize('content_platform', 'cetz', 'cetz', {
     host: 'localhost',
     dialect: 'mysql', // O el dialecto que estés utilizando
 });
